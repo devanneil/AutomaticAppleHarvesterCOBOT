c7401ef7ae19025216f21aba07205d1f924769d9
devanneil <devanneil2004@gmail.com>
2026-06-26 11:45:24 -0600

    Added esp32 relay checking support

--- esp_32_sketch/esp_32_suction_control/esp_32_suction_control.ino
+++ esp_32_sketch/esp_32_suction_control/esp_32_suction_control.ino
@@ -1,6 +1,7 @@
 const uint8_t HEADER = 0xAA;
 
-const int relayPins[4] = {15, 16, 17, 18};
+const int relayPins[4] = {15, 16, 17, 18}; // 15, RX, TX, MO
+const int switchPins[4] = {19, 23, 21, 22}; // MI, SDL, 21, SCL
 uint8_t relayState = 0;
 
 void setup() {
@@ -9,6 +10,9 @@ void setup() {
   for (int i = 0; i < 4; i++) {
     pinMode(relayPins[i], OUTPUT);
     digitalWrite(relayPins[i], LOW);
+  }
+  for (int i = 0; i < 4; i++) {
+    pinMode(switchPins[i], INPUT_PULLUP);
   }
 }
 
@@ -19,12 +23,13 @@ void loop() {
     if (header == HEADER) {
       uint8_t state = Serial.read();
       applyRelayState(state);
-
-      // optional ACK
-      Serial.write(HEADER);
-      Serial.write(relayState);
     }
   }
+  uint8_t switchState = check_switch_state();
+
+  // optional ACK
+  Serial.write(HEADER);
+  Serial.write(relayState | (switchState));
 }
 
 void applyRelayState(uint8_t state) {
@@ -33,4 +38,18 @@ void applyRelayState(uint8_t state) {
   for (int i = 0; i < 4; i++) {
     digitalWrite(relayPins[i], (state >> i) & 0x01);
   }
+}
+
+uint8_t check_switch_state()
+{
+    uint8_t state = 0;
+
+    for (uint8_t i = 0; i < 4; i++) {
+        // Active-low switch (INPUT_PULLUP)
+        if (digitalRead(switchPins[i]) == LOW) {
+            state |= (1 << i);
+        }
+    }
+
+    return state;
 }
